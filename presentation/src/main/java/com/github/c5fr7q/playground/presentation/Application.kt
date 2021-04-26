fb452a0f7e98ebfaeee25ff8ff9c096e7e5ff5cc
Vyacheslav Vorozheykin <qwe2rty1@gmail.com>
2021-04-26 18:47:31 +0700

    Provide compose navigation.

--- presentation/src/main/java/com/github/c5fr7q/playground/presentation/Application.kt
+++ presentation/src/main/java/com/github/c5fr7q/playground/presentation/Application.kt
@@ -1,18 +1,21 @@
 package com.github.c5fr7q.playground.presentation
 
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.github.c5fr7q.playground.presentation.theme.PlaygroundTheme
 import com.github.c5fr7q.playground.presentation.ui.fancy.FancyScreen
 
 @Composable
 fun Application() {
 	PlaygroundTheme {
-		// A surface container using the 'background' color from the theme
-		Surface(color = MaterialTheme.colors.background) {
-			FancyScreen(viewModel())
+		val navController = rememberNavController()
+		NavHost(navController = navController, startDestination = "fancy_screen") {
+			composable("fancy_screen") {
+				FancyScreen(viewModel())
+			}
 		}
 	}
 }
