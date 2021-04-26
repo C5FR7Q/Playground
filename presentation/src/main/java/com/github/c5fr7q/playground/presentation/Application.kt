57e9242639ec822e6a26353dcc972bb5b045171b
Vyacheslav Vorozheykin <qwe2rty1@gmail.com>
2021-04-26 18:52:15 +0700

    Provide Hilt Navigation.

--- presentation/src/main/java/com/github/c5fr7q/playground/presentation/Application.kt
+++ presentation/src/main/java/com/github/c5fr7q/playground/presentation/Application.kt
@@ -1,7 +1,7 @@
 package com.github.c5fr7q.playground.presentation
 
 import androidx.compose.runtime.Composable
-import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.hilt.navigation.compose.hiltNavGraphViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -14,7 +14,7 @@ fun Application() {
 		val navController = rememberNavController()
 		NavHost(navController = navController, startDestination = "fancy_screen") {
 			composable("fancy_screen") {
-				FancyScreen(viewModel())
+				FancyScreen(hiltNavGraphViewModel())
 			}
 		}
 	}
